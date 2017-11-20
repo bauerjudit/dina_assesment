ec3b2a502020da41678cd5e7d97463c9866951d0
Judit Bauer <judit.bauer@yahoo.com>
2017-11-20 12:25:54 +0100

    Let user update and add new users.

--- src/UserListCtrl.js
+++ src/UserListCtrl.js
@@ -7,6 +7,31 @@ angular.module('Assessment', ['ui.router', 'angularUtils.directives.dirPaginatio
 		$scope.title = "Users";
 		$scope.userList = [];
 
+		$scope.updatedUser = function(updatedUser) {
+			var userId = updatedUser.id;
+
+	    	if ( updatedUser.status === false ) {
+	    		updatedUser.status = {
+	    			'status': 'locked'
+	    		};
+	    	} else {
+	    		updatedUser.status = {
+	    			'status': 'active'
+	    		};
+	    	}
+      		$http.put('http://js-assessment-backend.herokuapp.com/users/' + userId + '.json', updatedUser.status);
+		}
+
+		$scope.addNewUser = function(newUser) {
+			if ( newUser.status === false ) {
+	    		newUser.status = 'locked';
+	    	} else {
+	    		newUser.status = 'active';
+	    	}
+
+			return $http.post('http://js-assessment-backend.herokuapp.com/users.json', newUser);
+		}
+
 		$scope.getUserList = function() {
 			return $scope.userList;
 		}
@@ -19,11 +44,16 @@ angular.module('Assessment', ['ui.router', 'angularUtils.directives.dirPaginatio
 
 	    $scope.changeUserStatus = function(updatedUser) {
 	    	var userId = updatedUser.id;
+	    	console.log(updatedUser)
 
 	    	if ( updatedUser.status === false ) {
-	    		updatedUser.status = 'locked';
+	    		updatedUser.status = {
+	    			'status': 'locked'
+	    		};
 	    	} else {
-	    		updatedUser.status = 'active';
+	    		updatedUser.status = {
+	    			'status': 'active'
+	    		};
 	    	}
       		$http.put('http://js-assessment-backend.herokuapp.com/users/' + userId + '.json', updatedUser.status).then(fetchAllUsers);
     	}
--- src/router.js
+++ src/router.js
@@ -1,15 +1,11 @@
 'use strict';
 
 angular.module('Assesment', ['ui.router', 'angularUtils.directives.dirPagination'])
-	.config([ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-		//$urlRouterProvider.otherwise('/');
-		$stateProvider
-      		.state('frontpage', {
-        	url: '/',
-       		//templateUrl: './templates/userList.html',
-        	controller: 'UserListCtrl',
-	        data: {
-	          pageTitle: 'List users',
-	        },
+	.config([ '$stateProvider', function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise("/")
+      		.state('new', {
+      			name: 'new_user',
+	        	url: '/new',
+	       		template: '<div>ezazzz</div>'
       })
 	}]);
